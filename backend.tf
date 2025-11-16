6f537c88a9979673f7dfc181d7ba529d06b218b8
Victor Nwoke <victornwoke147@outlook.com>
2025-11-16 02:41:27 +0000

    test pipeline

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "vic-rg"
-    storage_account_name = "vicstorageacct960"
+    storage_account_name = "vicstorageacct18616"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
