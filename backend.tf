15c510faa3391f56d36d0d74fa245cb3dfded654
Victor Nwoke <victornwoke147@outlook.com>
2025-11-16 02:44:10 +0000

    test pipeline

--- backend.tf
+++ backend.tf
@@ -4,5 +4,7 @@ terraform {
     storage_account_name = "vicstorageacct18616"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
+    use_msi              = false
+    use_oidc             = false
   }
 }
